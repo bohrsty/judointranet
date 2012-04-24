6bc963530bbba601002c53a878a3b6b6a979199a
nb <nils@bohrs.net>
2012-04-24 07:12:44 +0200

    removed unused methods in AnnouncementView

--- lib/classes/class.AnnouncementView.php
+++ lib/classes/class.AnnouncementView.php
@@ -52,22 +52,10 @@ class AnnouncementView extends PageView {
 							'id' => crc32('AnnouncementView') // 3960320393
 						),
 						'secondlevel' => array(
-							1 => array(
-								'getid' => 'listall', 
-								'name' => 'class.AnnouncementView#connectnavi#secondlevel#listall',
-								'id' => crc32('AnnouncementView|listall'), // 4169844043
-								'show' => true
-							),
 							0 => array(
 								'getid' => 'new', 
 								'name' => 'class.AnnouncementView#connectnavi#secondlevel#new',
 								'id' => crc32('AnnouncementView|new'), // 3704676583
-								'show' => false
-							),
-							2 => array(
-								'getid' => 'details', 
-								'name' => 'class.AnnouncementView#connectnavi#secondlevel#details',
-								'id' => crc32('AnnouncementView|details'), // 3931860135
 								'show' => false
 							),
 							3 => array(
@@ -128,17 +116,6 @@ class AnnouncementView extends PageView {
 				
 				switch($this->get('id')) {
 					
-					case 'listall':
-						
-						// set contents
-						// title
-						$this->add_output(array('title' => $this->title(parent::lang('class.AnouncementView#init#listall#title'))));
-						// navi
-						$this->add_output(array('navi' => $this->navi(basename($_SERVER['SCRIPT_FILENAME']))));
-						// main-content
-						$this->add_output(array('main' => $this->listall()));
-					break;
-					
 					case 'new':
 						
 						// set contents
@@ -148,26 +125,6 @@ class AnnouncementView extends PageView {
 						$this->add_output(array('navi' => $this->navi(basename($_SERVER['SCRIPT_FILENAME']))));
 						// main-content
 						$this->add_output(array('main' => $this->new_entry()));
-					break;
-					
-					case 'details':
-						
-						// set contents
-						// title
-						$this->add_output(array('title' => $this->title(parent::lang('class.CalendarView#init#details#title'))));
-						// navi
-						$this->add_output(array('navi' => $this->navi(basename($_SERVER['SCRIPT_FILENAME']))));
-						// main-content
-						// if cid does not exist, error
-						if(Calendar::check_id($this->get('cid'))) {
-							$this->add_output(array('main' => $this->details($this->get('cid'))));
-						} else {
-							
-							// error
-							$errno = $GLOBALS['Error']->error_raised('CidNotExists','details',$this->get('cid'));
-							$GLOBALS['Error']->handle_error($errno);
-							$this->add_output(array('main' => $GLOBALS['Error']->to_html($errno)),true);
-						}
 					break;
 					
 					case 'edit':
@@ -241,222 +198,6 @@ class AnnouncementView extends PageView {
 			// navi
 			$this->add_output(array('navi' => $this->navi(basename($_SERVER['SCRIPT_FILENAME']))));
 		}
-	}
-	
-	
-	
-	
-	
-	
-	
-	/**
-	 * listall lists all calendarentries less/equal than $time in table (paged)
-	 * shows only entrys for which the user has sufficient rights
-	 * 
-	 * @param int $timeto unix-timestamp from that the entrys are shown
-	 * @param int $timefrom unix-timestamp from that the entrys are shown
-	 * @return void
-	 */
-	private function listall($timeto,$timefrom) {
-		
-		// prepare return
-		$output = $tr_out = $th_out = '';
-		
-		// read all entries
-		$calendars = $this->read_all_entries();
-		// check sort
-		$entries = array();
-		if($this->get('sort') !== false) {
-			
-			// check if entry is in sort
-			foreach($calendars as $id => $entry) {
-				
-				if(in_array($this->get('sort'),$entry->return_rights()->return_rights())) {
-					$entries[$id] = $entry;
-				}
-			}
-		} else {
-			$entries = $calendars;
-		}
-		
-		// get templates
-		// a
-		try {
-			$a = new HtmlTemplate('templates/a.tpl');
-		} catch(Exception $e) {
-			$GLOBALS['Error']->handle_error($e);
-		}
-		// table
-		try {
-			$table = new HtmlTemplate('templates/table.tpl');
-		} catch(Exception $e) {
-			$GLOBALS['Error']->handle_error($e);
-		}
-		// tr
-		try {
-			$tr = new HtmlTemplate('templates/tr.tpl');
-		} catch(Exception $e) {
-			$GLOBALS['Error']->handle_error($e);
-		}
-		// th
-		try {
-			$th = new HtmlTemplate('templates/th.tpl');
-		} catch(Exception $e) {
-			$GLOBALS['Error']->handle_error($e);
-		}
-		// td
-		try {
-			$td = new HtmlTemplate('templates/td.tpl');
-		} catch(Exception $e) {
-			$GLOBALS['Error']->handle_error($e);
-		}
-		// img
-		try {
-			$img = new HtmlTemplate('templates/img.tpl');
-		} catch(Exception $e) {
-			$GLOBALS['Error']->handle_error($e);
-		}
-		
-		// prepare th
-		$th_out .= $th->parse(array( // date
-				'th.params' => ' class="date"',
-				'th.content' => parent::lang('class.CalendarView#listall#TH#date')
-			));
-		$th_out .= $th->parse(array( // name
-				'th.params' => ' class="name"',
-				'th.content' => parent::lang('class.CalendarView#listall#TH#name')
-			));
-		// if loggedin show admin links
-		if($_SESSION['user']->loggedin() === true) {
-			$th_out .= $th->parse(array( // admin
-					'th.params' => ' class="admin"',
-					'th.content' => parent::lang('class.CalendarView#listall#TH#admin')
-				));
-		}
-		
-		// parse tr for th
-		$tr_out .= $tr->parse(array(
-				'tr.params' => '',
-				'tr.content' => $th_out)
-			);
-		
-		// walk through entries
-		$counter = 0;
-		foreach($entries as $no => $entry) {
-			
-			// check if valid
-			if($entry->return_valid() == 1) {
-					
-				// check timefrom and timeto
-				if($entry->return_date('U') > $timefrom && $entry->return_date('U') <= $timeto) {
-					
-					// odd or even
-					if($counter%2 == 0) {
-						// even
-						$tr_params = ' class="calendar.listall.tr even"';
-					} else {
-						// odd
-						$tr_params = ' class="calendar.listall.tr odd"';
-					}
-					
-					// prepare name-link
-					$a_out = $a->parse(array(
-							'a.params' => '',
-							'a.href' => 'calendar.php?id=details&cid='.$entry->return_id(),
-							'a.title' => $entry->return_name(),
-							'a.content' => $entry->return_name()
-						));
-					
-					// prepare td
-					$td_out = $td->parse(array( // date
-							'td.params' => ' class="date"',
-							'td.content' => $entry->return_date('d.m.Y')
-						));
-					$td_out .= $td->parse(array( // name
-							'td.params' => '',
-							'td.content' => $a_out
-						));
-						
-					// add admin
-					// get intersection of user-groups and rights
-					$intersect = array_intersect(array_keys($_SESSION['user']->return_all_groups()),$entry->return_rights()->return_rights());
-					$admin = false;
-					// check if $intersect has values other than 0
-					foreach($intersect as $num => $igroup) {
-						if($igroup != 0) {
-							$admin = true;
-							break;
-						}
-					}
-					
-					// if $admin is true add admin-links
-					if($admin === true) {
-						
-						// prepare edit
-						// prepare img
-						$img_out = $img->parse(array(
-								'img.src' => 'img/edit.png',
-								'img.alt' => parent::lang('class.CalendarView#listall#alt#edit'),
-								'img.params' => 'title="'.parent::lang('class.CalendarView#listall#title#edit').'"'
-							));
-						
-						// prepare edit-link
-						$a_out = $a->parse(array(
-								'a.params' => '',
-								'a.href' => 'calendar.php?id=edit&cid='.$entry->return_id(),
-								'a.title' => parent::lang('class.CalendarView#listall#title#edit'),
-								'a.content' => $img_out
-							));
-							
-						// prepare delete
-						// prepare img
-						$img_out = $img->parse(array(
-								'img.src' => 'img/delete.png',
-								'img.alt' => parent::lang('class.CalendarView#listall#alt#delete'),
-								'img.params' => 'title="'.parent::lang('class.CalendarView#listall#title#delete').'"'
-							));
-						
-						// prepare delete-link
-						$a_out .= $a->parse(array(
-								'a.params' => '',
-								'a.href' => 'calendar.php?id=delete&cid='.$entry->return_id(),
-								'a.title' => parent::lang('class.CalendarView#listall#title#delete'),
-								'a.content' => $img_out
-							));
-						// prepare td
-						$td_out .= $td->parse(array( // admin
-								'td.params' => ' class="admin"',
-								'td.content' => $a_out
-							));
-						// if no announcement (preset_id==0), choose preset
-						if($entry->return_preset_id() == 0) {
-							
-							// create form
-							$td_out .= $this->read_preset_form($entry->return_id());
-						}
-					}
-					
-					// prepare tr
-					$tr_out .= $tr->parse(array(
-							'tr.params' => $tr_params,
-							'tr.content' => $td_out
-						));
-					
-					// increment counter
-					$counter++;
-				}
-			} else {
-				
-				// deleted items
-			}
-		}
-		
-		// complete table
-		$output = $table->parse(array(	'table.params' => ' id="calendar.listall"',
-										'table.content' => $tr_out));
-		
-		// return
-		return $output;
 	}
 	
 	
@@ -612,44 +353,6 @@ class AnnouncementView extends PageView {
 				$GLOBALS['Error']->handle_error($errno);
 				return $GLOBALS['Error']->to_html($errno);
 			}
-		} else {
-			
-			// error
-			$errno = $GLOBALS['Error']->error_raised('NotAuthorized','entry:'.$this->get('id'),$this->get('id'));
-			$GLOBALS['Error']->handle_error($errno);
-			return $GLOBALS['Error']->to_html($errno);
-		}
-	}
-	
-	
-	
-	
-	
-	
-	
-	/**
-	 * details returns the details of a calendar-entry as html-string
-	 * 
-	 * @param int $cid entry-id for calendar
-	 * @return string html-string with the details of the calendar entry
-	 */
-	private function details($cid) {
-	
-		// check rights
-		if(Rights::check_rights($cid,'calendar',true)) {
-				
-			// get calendar-object
-			$calendar = new Calendar($cid);
-			
-			// read template
-			try {
-				$calendar_details = new HtmlTemplate('templates/calendar.details.tpl');
-			} catch(Exception $e) {
-				$GLOBALS['Error']->handle_error($e);
-			}
-			
-			// return html-string
-			return $calendar->details_to_html($calendar_details);
 		} else {
 			
 			// error
